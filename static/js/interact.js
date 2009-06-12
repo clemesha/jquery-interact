b4817e7917ab83710ef59b9a38f68a5ac5c54f71
Alex Clemesha <clemesha@gmail.com>
2009-06-12 00:39:23 -0700

    Added an example of combining a jQuery slider with a flot plot. Each slide-release of the slider evaluates a hardcoded function of the new range defined by where the slider is released.

--- static/js/interact.js
+++ static/js/interact.js
@@ -1,3 +1,53 @@
+/*
+
+Interact
+========
+Create well defined API points to plug/mix-and-match:
+    * Input type (Javascript OR Python / Sage )
+    * Plotting Type [Client (e.g. flot) or Server (e.g. matplotlib)]
+
+All Input Forms and Sliders *must* be created in Client (javascript).
+
+
+Interact for Javascript
+=======================
+
+Example 
+--------
+
+--------- Javascript -----------
+<pre id="elem">
+
+function bar(a, x) {
+  return Math.sin(x*(1 + a*x));
+}
+interact(plot(bar, {"x":[0, 6]}), "a":[0, 2]})
+
+</pre>
+
+--------- Javascript -----------
+$("#elem").interact({language:javascript"});
+
+
+Steps need to create interact
+------------------------------
+#1 inspect of definition args => find function by 'interact' call. 
+#2 inspect callee args: === MUST be Object?, must have at least 1 var be list==== 
+#3 cache vals from #1,#2
+#4 create Input Forms and Sliders based on #1 and set vals with #2
+#5 set event handlers on #4
+#6 handle events be re-evaluting cache input function with new args+ranges
+
+
+Interact for Python
+===================
+
+TODO
+
+
+
+*/
+
 Function.prototype.method = function(name, func) {
     this.prototype[name] = func;
     return this;
@@ -24,6 +74,11 @@ Function.method('after', function(afterFunc) {
     };
 });
 
+
+/*
+
+*/
+
 (function($) {
 
     function Interact(target, options_) {
@@ -33,16 +88,12 @@ Function.method('after', function(afterFunc) {
         var inid = target.attr("id") + "_interact_input";
         var evalid = target.attr("id") + "_interact_eval";
         var outid = target.attr("id") + "_interact_output";
-        var plot = this;
         /* public functions */
-        plot.run = run;
+        this.plot = _plot;
+        this.interact = _interact;
     
         /* initialize */
         init(target, options_);
-
-        function run() {
-            console.log("run ", this);
-        };
 
         function init(target, opts){
             $.extend(options, opts); //update default options with user options.
@@ -62,16 +113,31 @@ Function.method('after', function(afterFunc) {
             $("#"+evalid).dblclick(do_interact);
         };
 
+        function plot(func, args) {
+            console.log("plot args: ", arguments);
+            console.log(typeof(func), typeof(args));
+            return function(){}
+        };
+
+        function func_data_list(func, min, max, step) {
+            /* Return list of (i, func(i)) for
+            a given range, with a given step size*/
+            var data = []; 
+            for (var i = min; i < max; i += step) {
+                data.push([i, func(i)]); 
+            };
+            return data;
+        };
+
+        function _interact() {
+            console.log("interact args: ", arguments);
+        };
+
         function do_interact(evt){
+            var f = $(evt.target).val();
             console.log("do_interact", evt);
-            /*  
-                #1 inspect of definition args 
-                #2 inspect callee args 
-                #3 cache vals from #1,#2
-                #4 create _input_boxs and _sliders_ based on #1 and set vals with #2
-                #5 set event handlers on #4
-                #6 handle events be re-evaluting cache input function with new args+ranges
-            */
+            console.log("newf", newf);
+            console.log("getargspec ", getargspec(newf));
         };
 
         function getargspec(func){
@@ -87,19 +153,9 @@ Function.method('after', function(afterFunc) {
         };
 
         function inspect(evt){
-            /*
-            function bar(vars) {
-              return Math.sin(vars.x) + Math.cos(vars.y);
-            }
-        
-            bar(0, 1) // display just input fields
-            bar(0, 1, {q=[1, 100], r=[5, 10]}) // vars and ranges
-            */
             var f = $(evt.target).val();
             try {
-                eval("newf = "+f);
-                console.log("getargspec ", getargspec(newf));
-                console.log("inspect ", f, newf);
+                eval("newf = "+f); //XXX eval and inspect namespace to get name
             } catch(exception){
                 console.warn("eval failed.  exception: ", exception);
             }
